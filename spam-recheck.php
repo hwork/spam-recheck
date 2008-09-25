054ec26afbc2a60da48f3f11af5fe51059e0268c
Henry Work <henry@techcrunch.com>
2008-09-25 12:05:39 -0700

    * works, sorta

--- spam-recheck.php
+++ spam-recheck.php
@@ -19,89 +19,34 @@ function recheck_config_page() {
 	
 	if ( isset($_POST['recheck_update']) ) {
 		/* do import */
-		//$posts = query_posts('showposts=500&offset=8500');	
-		
-		$comments_count = $wpdb->get_var("SELECT COUNT(*) FROM $wpdb->comments;");
-		//echo 'user count is ' . $comments_count;
-		
-		$first_comment_id = $wpdb->get_var("SELECT comment_id FROM $wpdb->comments ORDER BY comment_id ASC LIMIT 1");
-		echo 'first comment id is ' . $first_comment_id . "\n";
+		$comments = $wpdb->get_results("SELECT comment_ID FROM $wpdb->comments	WHERE comment_approved = '1'", ARRAY_A);
 
-		$last_comment_id = $wpdb->get_var("SELECT comment_id FROM $wpdb->comments ORDER BY comment_id DESC LIMIT 1");
-		echo 'last comment id is ' . $last_comment_id . "\n";
-
-		//echo 'first comment id is ' . $first_comment_id;
-		
-		$j = 0;
-		for ($i = 421740; $i < 421740+10; $i++) {
+		$spam_found = 0;
+		$comments_processed = 0;
+		foreach ($comments as $c) {
+			$i = $c['comment_ID'];
 			$comment = $wpdb->get_row("SELECT * FROM $wpdb->comments WHERE comment_id = $i", ARRAY_A);
-			if ($comment) {
-				if ($comment['comment_approved'] != '1') {
-					if ($comment['comment_approved'] == '0') {
-						echo '<p>comment #' . $comment['comment_ID'] . ' is not approved, skipping</p>';
-					}
-					else if ($comment['comment_approved'] == 'spam') {
-				}
-				else {
-					$j++;				
-					echo '<p>comment #' . $comment['comment_ID'] . ' is going to akismet ... ';
-					$comment['user_ip']    = $comment['comment_author_IP'];
-					$comment['user_agent'] = $comment['comment_agent'];
-					$comment['referrer']   = '';
-					$comment['blog']       = 'http://www.techcrunch.com/';
-					$id = (int) $comment['comment_ID'];
-
-					$query_string = '';
-					foreach ( $comment as $key => $data )
-					$query_string .= $key . '=' . urlencode( stripslashes($data) ) . '&';
-
-					$response = akismet_http_post($query_string, $akismet_api_host, '/1.1/comment-check', $akismet_api_port);
-
-					if ($response[1] == 'true') {
-						echo 'and is INDEED SPAM!!!, marking as such';
-					}
-					else {
-						echo 'and is not spam, continuing';
-					}
-					echo '</p>';
-
-					usleep(500000);
-				}
+			$comment['user_ip']    = $comment['comment_author_IP'];
+			$comment['user_agent'] = $comment['comment_agent'];
+			$comment['referrer']   = '';
+			$comment['blog']       = get_option('home');
+			$id = (int) $comment['comment_ID'];					
+      
+			$query_string = '';
+			foreach ( $comment as $key => $data )
+			$query_string .= $key . '=' . urlencode( stripslashes($data) ) . '&';
+      
+			$response = akismet_http_post($query_string, $akismet_api_host, '/1.1/comment-check', $akismet_api_port);
+      
+			if ($response[1] == 'true') {
+				//echo '<p>comment #' . $comment['comment_ID'] . ' is SPAM!!!</p>';
+				$wpdb->query( "UPDATE $wpdb->comments SET comment_approved = '0' WHERE comment_ID = $id" );
+				$spam_found++;
 			}
+			$comments_processed++;
 		}
-		
-		//$moderation = $wpdb->get_results( "SELECT * FROM $wpdb->comments WHERE comment_approved = '1' LIMIT 10", ARRAY_A );
-		//foreach ( (array) $moderation as $c ) {
-		//	$c['user_ip']    = $c['comment_author_IP'];
-		//	$c['user_agent'] = $c['comment_agent'];
-		//	$c['referrer']   = '';
-		//	$c['blog']       = get_option('home');
-		//	$id = (int) $c['comment_ID'];
-    //
-		//	$query_string = '';
-		//	foreach ( $c as $key => $data )
-		//	$query_string .= $key . '=' . urlencode( stripslashes($data) ) . '&';
-    //
-		//	$response = akismet_http_post($query_string, $akismet_api_host, '/1.1/comment-check', $akismet_api_port);
-		//	
-		//	echo $response[1];
-		//	echo "\n";
-		//
-		//	usleep(500000);
-		//}
-    //
-    //
-		//	if ( 'true' == $response[1] ) {
-		//		$wpdb->query( "UPDATE $wpdb->comments SET comment_approved = 'spam' WHERE comment_ID = $id" );
-		//	}
-		//}
-		
-		//foreach ( $posts as $post ) {
-		//	if ($post->post_status == 'publish') {
-		//		cb_publish_post($post->ID);
-		//		usleep(200000);
-		//	}
-		//}
+		echo '<p>total spam found: ' . $spam_found . '</p>';
+		echo '<p>total comments processed: ' . $comments_processed . '</p>';
 	?>
 		<div id="message" class="updated fade">
 			<p>Victory!</p>
